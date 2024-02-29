5f14d9ad8bc5a015f81ed188a22ca2f3182b6dbc
rishikesh-itobuz <rishikesh@itobuz.com>
2024-02-29 11:28:45 +0530

    configure the enviroment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // to make it available for every module without importing it
+      envFilePath: '.env',
+      // to configure another custom env file path ==> replace the envFilePath
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
